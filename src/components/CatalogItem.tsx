698542977cb4f6dab82400f91fe21eaa2fdaab13
Rafael <rteles78@gmail.com>
2021-05-11 11:24:58 -0300

    first commit

---
+++ src/components/CatalogItem.tsx
@@ -0,0 +1,39 @@
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { IState } from '../store';
+import { addProductToCartRequest } from '../store/modules/cart/actions';
+import { IProduct } from '../store/modules/cart/types';
+
+interface CatalogItemProps {
+  product: IProduct;
+}
+
+const CatalogItem: React.FC<CatalogItemProps> = ({ product }) => {
+  const dispatch = useDispatch();
+
+  const hasFailureStockCheck = useSelector<IState, boolean>(state => {
+    return state.cart.failureStockCheck.includes(product.id);
+  });
+
+  const handleAddProductToCart = useCallback(() => {
+    dispatch(addProductToCartRequest(product));
+  }, [dispatch, product]);
+  
+  return (
+    <article key={product.id}>
+      <strong>{product.title}</strong> {" - "}
+      <span>{product.price}</span> {" - "}          
+
+      <button 
+        type="button"
+        onClick={handleAddProductToCart}
+      >
+          Comprar
+      </button>
+
+      { hasFailureStockCheck && <span style={{ color: 'red' }}>Falta de estoque</span>}
+    </article>
+  );
+}
+
+export default CatalogItem;
---
+++ src/store/modules/cart/sagas.ts
@@ -0,0 +1,35 @@
+import { AxiosResponse } from 'axios';
+import { all, call, put, select, takeLatest } from 'redux-saga/effects';
+import { IState } from '../..';
+import api from '../../../services/api';
+import { addProductToCartRequest, addProductToCartSuccess, addProductToCartFailure } from './actions';
+import { ActionTypes } from './types';
+
+type CheckProdutStockRequest = ReturnType<typeof addProductToCartRequest>;
+
+interface IStockResponse {
+  id: number;
+  quantity: number;
+}
+
+function* checkProductStock({ payload }: CheckProdutStockRequest) {
+  const { product } = payload;  
+
+  const currentQuantity: number = yield select((state: IState) => {
+    return state.cart.items.find(item => item.product.id === product.id)?.quantity ?? 0;
+  });
+
+  const availableStockResponse: AxiosResponse<IStockResponse> = yield call(api.get, `stock/${product.id}`);
+
+  if (availableStockResponse.data.quantity > currentQuantity) {
+    yield put(addProductToCartSuccess(product));
+  } else {
+    yield put(addProductToCartFailure(product.id));
+  }
+
+  console.log(currentQuantity);
+}
+
+export default all([
+  takeLatest(ActionTypes.addProductToCartRequest, checkProductStock)
+]);
